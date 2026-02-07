31af99cea78d302f16cb15c962b7d90468040ed5
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-02-07 18:05:43 -0500

    garble the pe files

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+#![no_std]
+
+use alloc::collections::btree_set::BTreeSet;
+use pelite::pe64::{Pe, PeView};
+extern crate alloc;
+pub fn parble(buffer: &mut [u8], mut hide_import: impl FnMut(&mut [u8])) {
+    let pe = PeView::from_bytes(buffer).unwrap();
+    let mut to_hide = BTreeSet::new();
+    let mut import = pe.imports().unwrap();
+    for imp in import.iter() {
+        let slice = pe.derva_slice_s(imp.image().OriginalFirstThunk, 0).unwrap();
+        for va in slice.iter() {
+            to_hide.insert(pe.rva_to_file_offset(*va).unwrap());
+        }
+    }
+    for name in pe.exports().unwrap().names().unwrap() {
+        to_hide.insert(pe.rva_to_file_offset(*name).unwrap());
+    }
+    for hide in to_hide {
+        let offset = hide as usize;
+        let mut slice = &mut buffer[offset..offset + 1];
+        hide_import(&mut slice);
+    }
+}
